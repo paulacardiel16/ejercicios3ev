c4338b09098cc7826c74a4767e20b971c8657775
paulacardiel16 <43232610+paulacardiel16@users.noreply.github.com>
2019-05-02 09:14:33 +0200

    Delete punteros3.cpp

--- punteros3.cpp
+++
@@ -1,9 +0,0 @@
-//Programas sobre manejo de memoria
-#include<stdio.h>
-int main(){
-    int hermanos=3;
-    int hermanas=2;
-    int salir;
-    printf("tengo %i hermanas y %i hermanos",hermanas,hermanos);
-    scanf("%c",&salir);
-}
